dcda2bc2034562d3667daf0c98929edf204f40c0
kennywang710 <62987623+kennywang710@users.noreply.github.com>
2020-04-10 11:32:27 +0800

    Update sync_file02.py
    add three lines

--- sync_file02.py
+++ sync_file02.py
@@ -5,3 +5,6 @@ print("modify on local");
 print("modify on local");
 print("modify on local");
 print("modify on local");
+print("modify on hub1");
+print("modify on hub2");
+print("modify on hub3");
